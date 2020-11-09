2f1e59ee045841197de0acc302ca7a2995efd839
Ardi <rizkyardimaulana@gmail.com>
2020-11-10 01:18:07 +0700

    changing shift assignment

--- BarrelShifter.v
+++ BarrelShifter.v
@@ -6,9 +6,9 @@ module BarrelShifter(op, shift_mag, result);
   wire [15:0] mux2_out;
   wire [15:0] mux3_out;
   
-  mux mux_instance1(shift_mag[0], op, (op << 1), mux1_out);
-  mux mux_instance2(shift_mag[1], mux1_out, (mux1_out << 2), mux2_out);
-  mux mux_instance3(shift_mag[2], mux2_out, (mux2_out << 4), mux3_out);
-  mux mux_instance4(shift_mag[3], mux3_out, (mux3_out << 8), result);
+  mux mux_instance1(shift_mag[0], op, (op >> 1), mux1_out);
+  mux mux_instance2(shift_mag[1], mux1_out, (mux1_out >> 2), mux2_out);
+  mux mux_instance3(shift_mag[2], mux2_out, (mux2_out >> 4), mux3_out);
+  mux mux_instance4(shift_mag[3], mux3_out, (mux3_out >> 8), result);
   
 endmodule
