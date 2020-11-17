ea4b1a4b14725886c4ab137c3c9a3ac31c52e769
Ardi <rizkyardimaulana@gmail.com>
2020-11-17 11:22:52 +0700

    resolve state issues

--- q-learning_accelerator.v
+++ q-learning_accelerator.v
@@ -3,31 +3,30 @@
 module Q_learning_acc(clk, en, action, state, next_state,  reward, gamma, alpha, result);
   input clk,en;
   input[3:0] action;
-  input [5:0] state, next_state; //size?
+  input [5:0] state;
+  input [5:0] next_state; //size?
   input [15:0] reward; 
   input [15:0] gamma;
   input [15:0] alpha;
   input [15:0] result;
 
   //wiring
-  wire[15:0] out_ram_1,
-             out_ram_2,
-             out_ram_3,
-             out_ram_4,
-             out_ram_5,
-             out_ram_6,
-             out_ram_7,
-             out_ram_8,
-             out_ram_9,
-             out_ram_10,
-             out_ram_11,
-             out_ram_12,
-             out_ram_13,
-             out_ram_14,
-             out_ram_15;
-    
-    wire [3:0] state_addr,
-               next_state_addr;
+  wire[15:0] out_ram_1, out_delay_1,
+             out_ram_2, out_delay_2,
+             out_ram_3, out_delay_3,
+             out_ram_4, out_delay_4,
+             out_ram_5, out_delay_5,
+             out_ram_6, out_delay_6,
+             out_ram_7, out_delay_7,
+             out_ram_8, out_delay_8,
+             out_ram_9, out_delay_9,
+             out_ram_10, out_delay_10,
+             out_ram_11, out_delay_11,
+             out_ram_12, out_delay_12,
+             out_ram_13, out_delay_13,
+             out_ram_14, out_delay_14,
+             out_ram_15, out_delay_15;
+  
 
     //decoder to ram wire
     wire en1,
@@ -58,142 +57,204 @@ module Q_learning_acc(clk, en, action, state, next_state,  reward, gamma, alpha,
   //module instantiation
     action_ram ram_1(.clk(clk), 
                    .en(en), 
-                   .wr_addr(state_addr),
-                   .rd_addr(next_state_addr),
+                   .wr_addr(state),
+                   .rd_addr(next_state),
                    .write_en(en1),
                    .data_in(new_q_value),
                    .data_out(out_ram_1));
 
     action_ram ram_2(.clk(clk), 
                    .en(en), 
-                   .wr_addr(state_addr),
-                   .rd_addr(next_state_addr),
+                   .wr_addr(state),
+                   .rd_addr(next_state),
                    .write_en(en2),
                    .data_in(new_q_value),
                    .data_out(out_ram_2));               
 
     action_ram ram_3(.clk(clk), 
                    .en(en), 
-                   .wr_addr(state_addr),
-                   .rd_addr(next_state_addr),
+                   .wr_addr(state),
+                   .rd_addr(next_state),
                    .write_en(en3),
                    .data_in(new_q_value),
                    .data_out(out_ram_3));
 
     action_ram ram_4(.clk(clk), 
                    .en(en), 
-                   .wr_addr(state_addr),
-                   .rd_addr(next_state_addr),
+                   .wr_addr(state),
+                   .rd_addr(next_state),
                    .write_en(en4),
                    .data_in(new_q_value),
                    .data_out(out_ram_4));
 
     action_ram ram_5(.clk(clk), 
                    .en(en), 
-                   .wr_addr(state_addr),
-                   .rd_addr(next_state_addr),
+                   .wr_addr(state),
+                   .rd_addr(next_state),
                    .write_en(en5),
                    .data_in(new_q_value),
                    .data_out(out_ram_5));
 
     action_ram ram_6(.clk(clk), 
                    .en(en), 
-                   .wr_addr(state_addr),
-                   .rd_addr(next_state_addr),
+                   .wr_addr(state),
+                   .rd_addr(next_state),
                    .write_en(en6),
                    .data_in(new_q_value),
                    .data_out(out_ram_6));
 
     action_ram ram_7(.clk(clk), 
                    .en(en), 
-                   .wr_addr(state_addr),
-                   .rd_addr(next_state_addr),
+                   .wr_addr(state),
+                   .rd_addr(next_state),
                    .write_en(en7),
                    .data_in(new_q_value),
                    .data_out(out_ram_7));
 
     action_ram ram_8(.clk(clk), 
                    .en(en), 
-                   .wr_addr(state_addr),
-                   .rd_addr(next_state_addr),
+                   .wr_addr(state),
+                   .rd_addr(next_state),
                    .write_en(en8),
                    .data_in(new_q_value),
                    .data_out(out_ram_8));
 
     action_ram ram_9(.clk(clk), 
                    .en(en), 
-                   .wr_addr(state_addr),
-                   .rd_addr(next_state_addr),
+                   .wr_addr(state),
+                   .rd_addr(next_state),
                    .write_en(en9),
                    .data_in(new_q_value),
                    .data_out(out_ram_9));
 
     action_ram ram_10(.clk(clk), 
                    .en(en), 
-                   .wr_addr(state_addr),
-                   .rd_addr(next_state_addr),
+                   .wr_addr(state),
+                   .rd_addr(next_state),
                    .write_en(en10),
                    .data_in(new_q_value),
                    .data_out(out_ram_10));
 
     action_ram ram_11(.clk(clk), 
                    .en(en), 
-                   .wr_addr(state_addr),
-                   .rd_addr(next_state_addr),
+                   .wr_addr(state),
+                   .rd_addr(next_state),
                    .write_en(en11),
                    .data_in(new_q_value),
                    .data_out(out_ram_11));
 
     action_ram ram_12(.clk(clk), 
                    .en(en), 
-                   .wr_addr(state_addr),
-                   .rd_addr(next_state_addr),
+                   .wr_addr(state),
+                   .rd_addr(next_state),
                    .write_en(en12),
                    .data_in(new_q_value),
                    .data_out(out_ram_12));
 
     action_ram ram_13(.clk(clk), 
                    .en(en), 
-                   .wr_addr(state_addr),
-                   .rd_addr(next_state_addr),
+                   .wr_addr(state),
+                   .rd_addr(next_state),
                    .write_en(en13),
                    .data_in(new_q_value),
                    .data_out(out_ram_13));
 
     action_ram ram_14(.clk(clk), 
                    .en(en), 
-                   .wr_addr(state_addr),
-                   .rd_addr(next_state_addr),
+                   .wr_addr(state),
+                   .rd_addr(next_state),
                    .write_en(en14),
                    .data_in(new_q_value),
                    .data_out(out_ram_14));
     
     action_ram ram_15(.clk(clk), 
                    .en(en), 
-                   .wr_addr(state_addr),
-                   .rd_addr(next_state_addr),
+                   .wr_addr(state),
+                   .rd_addr(next_state),
                    .write_en(en15),
                    .data_in(new_q_value),
                    .data_out(out_ram_15));
 
+    //Delay
+    Delay delay_1(.clk(clk),
+                 .din(out_ram_1),
+                 .dout(out_delay_1));
+
+    Delay delay_2(.clk(clk),
+                 .din(out_ram_2),
+                 .dout(out_delay_2));
+    
+    Delay delay_3(.clk(clk),
+                 .din(out_ram_3),
+                 .dout(out_delay_3));
+
+    Delay delay_4(.clk(clk),
+                 .din(out_ram_4),
+                 .dout(out_delay_4));
+    
+    Delay delay_5(.clk(clk),
+                 .din(out_ram_5),
+                 .dout(out_delay_5));
+
+    Delay delay_6(.clk(clk),
+                 .din(out_ram_6),
+                 .dout(out_delay_6));
+
+    Delay delay_7(.clk(clk),
+                 .din(out_ram_7),
+                 .dout(out_delay_7));
+
+    Delay delay_8(.clk(clk),
+                 .din(out_ram_8),
+                 .dout(out_delay_8));
+
+    Delay delay_9(.clk(clk),
+                 .din(out_ram_9),
+                 .dout(out_delay_9));
+
+    Delay delay_10(.clk(clk),
+                 .din(out_ram_10),
+                 .dout(out_delay_10));
+
+    Delay delay_11(.clk(clk),
+                 .din(out_ram_11),
+                 .dout(out_delay_11));
+
+    Delay delay_12(.clk(clk),
+                 .din(out_ram_12),
+                 .dout(out_delay_12));
+    
+    Delay delay_13(.clk(clk),
+                 .din(out_ram_13),
+                 .dout(out_delay_13));
+
+    Delay delay_14(.clk(clk),
+                 .din(out_ram_14),
+                 .dout(out_delay_14));
+
+    Delay delay_15(.clk(clk),
+                 .din(out_ram_15),
+                 .dout(out_delay_15));
+
+
     //multiplexer 16 to 1
     Mux16to1 mux16to1(.sel(action),
-                         .d1(out_ram_1),
-                         .d2(out_ram_2),
-                         .d3(out_ram_3),
-                         .d4(out_ram_4),
-                         .d5(out_ram_5),
-                         .d6(out_ram_6),
-                         .d7(out_ram_7),
-                         .d8(out_ram_8),
-                         .d9(out_ram_9),
-                         .d10(out_ram_10),
-                         .d11(out_ram_11),
-                         .d12(out_ram_12),
-                         .d13(out_ram_13),
-                         .d14(out_ram_14),
-                         .d15(out_ram_15), 
-                         .dout(q_value_selected));
+                      .d1(out_delay_1),
+                      .d2(out_delay_2),
+                      .d3(out_delay_3),
+                      .d4(out_delay_4),
+                      .d5(out_delay_5),
+                      .d6(out_delay_6),
+                      .d7(out_delay_7),
+                      .d8(out_delay_8),
+                      .d9(out_delay_9),
+                      .d10(out_delay_10),
+                      .d11(out_delay_11),
+                      .d12(out_delay_12),
+                      .d13(out_delay_13),
+                      .d14(out_delay_14),
+                      .d15(out_delay_15), 
+                      .dout(q_value_selected));
 
 //this is new
     //Decoder
