1eb0bed52dc3365c6eaf9d57764500b67be4e254
Ardi <rizkyardimaulana@gmail.com>
2020-11-24 11:37:39 +0700

    DelayAction -> DelayReward

--- Delay.v
+++ Delay.v
@@ -14,12 +14,12 @@ module DelayActionRAM(clk, din,dout);
   end
 endmodule
 
-module DelayAction(clk,din,dout);
+module DelayReward(clk,din,dout);
   input clk;
-  input [3:0] din;
-  output reg [3:0] dout;
+  input [15:0] din;
+  output reg [315:0] dout;
   //buffer register;
-  reg [3:0] temp1,temp2;
+  reg [15:0] temp1,temp2;
 
   always@(posedge clk) begin
       temp1 <= din;
