d539657f01ab10295ba3af9e52b697fd53fc0efd
devanandakrnads <73833967+devanandakrnads@users.noreply.github.com>
2020-11-24 13:19:06 +0700

    Update ControlUnit.v

--- ControlUnit.v
+++ ControlUnit.v
@@ -1,5 +1,5 @@
 
-module ControlUnit(clk, en, epsilon);
+module ControlUnit(clk,en,epsilon);
   input clk;
   input en;
   input epsilon; 
@@ -10,16 +10,16 @@ module ControlUnit(clk, en, epsilon);
   wire [5:0] w_current_state
   
   RewardGenerator Reward(.next_state(w_next_state),
-			 .next_reward(w_next_action));
+			 .next_reward(w_next_reward));
 
   StateSelector State(.next_action(w_next_action),
-			   .current_state(w_current_state), 
+	         	   .current_state(w_current_state), 
 			   .next_state(w_next_state));
 
-  QLearningAgent Agent(.clk(clk),
+  Q_LearningAgent Agent(.clk(clk),
 			.en(en),
 			.reward(w_next_reward), 	
-			.next_state(w_current_state));
+			.next_state(w_next_state));
 
   MazeMap Maze(.current_state(w_current_state)));
  
