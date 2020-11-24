4f94c5ce2a9d8dab375a435da8bdace183a60f26
Ardi <rizkyardimaulana@gmail.com>
2020-11-24 14:42:49 +0700

    reduce reward delay to 1 clock

--- Delay.v
+++ Delay.v
@@ -3,7 +3,7 @@
 module DelayActionRAM(clk, din,dout);
   input clk;
   input [15:0] din;
-  output reg [15:0] dout;
+  output reg [15:0] dout = 16'd0;
 
   //buffer register;
   reg [15:0] temp1,temp2,temp3;
@@ -17,20 +17,20 @@ endmodule
 module DelayReward(clk,din,dout);
   input clk;
   input [15:0] din;
-  output reg [315:0] dout;
+  output reg [15:0] dout = 16'd0;
   //buffer register;
   reg [15:0] temp1,temp2;
 
   always@(posedge clk) begin
-      temp1 <= din;
-      dout  <= temp1;
+      //temp1 <= din;
+      dout  <= din;
   end
 endmodule
 
 module DelayState(clk,din,dout);
   input clk;
   input [5:0] din;
-  output reg [3:0] dout;
+  output reg [5:0] dout = 5'd0;
   //buffer register;
   reg [5:0] temp1,temp2;
 
