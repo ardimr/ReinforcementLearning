5c13f62473ec819dbb73156b4577f4262fcf4988
Ardi <rizkyardimaulana@gmail.com>
2020-11-24 14:43:15 +0700

    revisi QLearningAgent

--- QLearningAgent.v
+++ QLearningAgent.v
@@ -1,36 +1,38 @@
 //Module Q learning Agent
 
-module QLearningAgent (clk, en, next_reward, current_state, next_state);
+module QLearningAgent (clk, en, next_reward, next_state);
   input clk;
   input en;
   input[15:0] next_reward; // Reward
   input[5:0] next_state; // Next State
 
   wire [63:0] w_q_values;
-  wire w_next_reward;
-  wire w_next_state;
-  wire w_curr_reward;
-  wire w_curr_state;
+  //wire w_next_reward;
+  //wire w_next_state;
+  wire [15:0] w_curr_reward;
+  wire [5:0] w_curr_state;
+  wire [3:0] w_curr_action;
 
- 
-  QlearningAccelerator QLearningAccelerator(.clk(clk), 
-                    .en(en),
-                    .current_action(w_curr_action),
-                    .current_state(w_curr_state),
-                    .next_state(w_next_state),
-                    .current_reward(w_curr_reward),
-                    .Q_out_action(w_q_values));
+  reg [15:0] epsilon = 16'b0000000011100000;
+  QLearningAccelerator QLearningAccelerator(.clk(clk), 
+                                            .en(en),
+                                            .current_action(w_curr_action),
+                                            .current_state(w_curr_state),
+                                            .next_state(next_state),
+                                            .current_reward(w_curr_reward),
+                                            .Q_out_action(w_q_values));
 
    PolicyGenerator PolicyGenerator(.clk(clk),
-                                   .next_action(w_next_action),
+                                   .current_action(w_curr_action),
+                                   .epsilon(epsilon),
                                    .q_values(w_q_values));
     
     DelayReward DelayReward(.clk(clk),
-                            .din(w_next_reward),
+                            .din(next_reward),
                             .dout(w_curr_reward));
     
-    Delay_state delay_state(.clk(clk),
-                            .din(w_next_state),
+    DelayState DelayState(.clk(clk),
+                            .din(next_state),
                             .dout(w_curr_state));
 
 endmodule
