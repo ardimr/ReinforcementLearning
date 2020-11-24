3cbc33d9a71958bde4fc7585f130a015e0b93c38
LowlingLife <49157065+LowlingLife@users.noreply.github.com>
2020-11-24 13:16:10 +0700

    Update ControlUnit.v

--- ControlUnit.v
+++ ControlUnit.v
@@ -1,22 +1,22 @@
 
-module ControlUnit(clk,en,epselon,maze);
+module ControlUnit(clk, en, epsilon);
   input clk;
   input en;
-  input epselon 
+  input epsilon; 
   
   wire [3:0] w_next_action;
-  wire [5:0] w_next_reward;
+  wire [15:0] w_next_reward;
   wire [5:0] W_next_state;
   wire [5:0] w_current_state
   
   RewardGenerator Reward(.next_state(w_next_state),
-			 .next_reward(w_next _action));
+			 .next_reward(w_next_action));
 
-  PenentuState_Block State(.next_action(w_next_action),
+  StateSelector State(.next_action(w_next_action),
 			   .current_state(w_current_state), 
 			   .next_state(w_next_state));
 
-  Q_LearningAgent Agent(.clk(clk),
+  QLearningAgent Agent(.clk(clk),
 			.en(en),
 			.reward(w_next_reward), 	
 			.next_state(w_current_state));
