6b4cb70f22fac845ba359e5466362a715cb88d2f
Ardi <rizkyardimaulana@gmail.com>
2020-11-24 12:11:45 +0700

    update

--- PolicyGenerator.v
+++ PolicyGenerator.v
@@ -1,17 +1,17 @@
-module PolicyGenerator(clk, q_values, new_action, epsilon);
+module PolicyGenerator(clk, q_values, epsilon, current_action);
   input clk; // Clock
   input [15:0] epsilon;
   input [63:0] q_values; // Q Values untuk 1 State
-  output reg [3:0] next_action; // Next Action
+  output reg [3:0] current_action; // Next Action
   
-  wire [3:0] w_next_action; // Next Action Wire
+  wire [3:0] w_current_action; // Next Action Wire
   
-  ActionSelector Act(.q_values(q_values),.action(w_next_action), .epsilon(epsilon));
+  ActionSelector Act(.q_values(q_values), .epsilon(epsilon), .action(w_current_action));
   
   //Delay Action
   always@(posedge clk)
      begin
-        next_action = w_next_action;
+        current_action = w_current_action;
      end
   
 endmodule
