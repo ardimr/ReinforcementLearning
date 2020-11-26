35020512b95cd6d8252a099b61f850f9fed926e0
LowlingLife <49157065+LowlingLife@users.noreply.github.com>
2020-11-26 21:50:29 +0700

    Update QUpdater.v

--- QUpdater.v
+++ QUpdater.v
@@ -8,12 +8,14 @@ module QUpdater (old_Q, max_Q, reward, new_Q);
 	wire [15:0] max_j;
 	wire [15:0] max_k;
 	wire [15:0] combined_Q;
+	wire [15:0] final_Q;
 	
 	BarrelShifter barrel_max_i(max_Q, 4'd1, max_i);
 	BarrelShifter barrel_max_j(max_Q, 4'd2, max_j);
 	BarrelShifter barrel_max_k(max_Q, 4'd3, max_k);
-	BarrelShifter barrel_alpha(combined_Q, 4'd1, new_Q);
+	BarrelShifter barrel_alpha(combined_Q, 4'd1, final_Q);
 	
 	assign combined_Q = reward + max_i + max_j + max_k - old_Q;
+	assign new_Q = final_Q + old_Q;
 	
 endmodule
