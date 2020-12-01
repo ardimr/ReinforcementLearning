3e50e20ec4702d3b1bc4d60d7c92aa40f961e6f0
LowlingLife <49157065+LowlingLife@users.noreply.github.com>
2020-12-01 11:09:13 +0700

    Update PolicyGenerator.v

--- PolicyGenerator.v
+++ PolicyGenerator.v
@@ -1,12 +1,12 @@
-module PolicyGenerator(clk, q_values, epsilon, current_action);
-  input clk; // Clock
+module PolicyGenerator(clk, start, q_values, epsilon, current_action);
+  input clk, start; // Clock
   input [15:0] epsilon;
   input [63:0] q_values; // Q Values untuk 1 State
   output reg [3:0] current_action; // Next Action
   
   wire [3:0] w_current_action; // Next Action Wire
   
-  ActionSelector Act(.q_values(q_values), .epsilon(epsilon), .action(w_current_action));
+  ActionSelector Act(.clk(clk), .start(start), .q_values(q_values), .epsilon(epsilon), .action(w_current_action));
   
   //Delay Action
   always@(posedge clk)
