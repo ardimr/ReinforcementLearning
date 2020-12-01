0f06b8c4a876ea94985021df33efeff28b982139
devanandakrnads <73833967+devanandakrnads@users.noreply.github.com>
2020-12-01 14:44:10 +0700

    Update PolicyGenerator.v

--- PolicyGenerator.v
+++ PolicyGenerator.v
@@ -1,13 +1,17 @@
-module PolicyGenerator(clk, start, q_values, epsilon, current_action);
-  input clk, start; // Clock
+module PolicyGenerator(clk, start, q_values, epsilon, current_action,next_action);
+  input clk; // Clock
+  input start;
   input [15:0] epsilon;
   input [63:0] q_values; // Q Values untuk 1 State
   output reg [3:0] current_action; // Next Action
-  
+  output [3:0] next_action;
+
   wire [3:0] w_current_action; // Next Action Wire
   
-  ActionSelector Act(.clk(clk), .start(start), .q_values(q_values), .epsilon(epsilon), .action(w_current_action));
+  ActionSelector Act(.clk(clk),.start(start),.q_values(q_values), .epsilon(epsilon), .action(w_current_action));
   
+  assign next_action=w_current_action;
+
   //Delay Action
   always@(posedge clk)
      begin
